fc5aa55a0589648b60c5c658d57fad457f61fb38
Birs <birsanmadrian13@gmail.com>
2016-05-20 11:09:59 +0300

    added get coords

--- src/main/java/ro/ebs/internship/alzheimer/controller/CoordsController.java
+++ src/main/java/ro/ebs/internship/alzheimer/controller/CoordsController.java
@@ -4,6 +4,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ro.ebs.internship.alzheimer.entity.Coord;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @RequestMapping("coords")
 public class CoordsController {
@@ -16,6 +19,21 @@ public class CoordsController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createCoords(@PathVariable("patient") String patient, @RequestBody Coord coord){
         System.out.println(patient + " is at " + coord.toString());
+    }
+
+    @RequestMapping(
+            value = "caretakers/{caretaker}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public List<Coord> getCoords(@PathVariable("caretaker") String caretaker){
+        System.out.println(caretaker);
+        Coord coord = new Coord();
+        coord.setLatitude("45.98");
+        coord.setLongitude("25.89");
+        coord.setTimestamp(1234L);
+        return Collections.singletonList(coord);
     }
 
 }
