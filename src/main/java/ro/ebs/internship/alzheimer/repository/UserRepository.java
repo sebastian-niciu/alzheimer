c83b1e7b6f903cb19dc415daa9f0b148360c1187
Sheila <siegel.sheila@gmail.com>
2016-05-26 16:20:47 +0300

    UserRepository, User

--- src/main/java/ro/ebs/internship/alzheimer/repository/UserRepository.java
+++ src/main/java/ro/ebs/internship/alzheimer/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package ro.ebs.internship.alzheimer.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ro.ebs.internship.alzheimer.entity.User;
 
-
+@Repository
 public interface UserRepository  extends CrudRepository<User, String> {
 
 
